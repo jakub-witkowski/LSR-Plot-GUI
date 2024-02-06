508b9e62d1d898e600f4dc14284c8531a75eb9b7
jakub.witkowski <j.witkowski@poczta.onet.pl>
2024-02-06 19:44:29 +0100

    further work on UI and annotations to code

--- tpolynomial.h
+++ tpolynomial.h
@@ -1,7 +1,10 @@
 #ifndef TPOLYNOMIAL_H
 #define TPOLYNOMIAL_H
 
+/* including ROOT Framework class */
 #include "TF1.h"
+
+/* STL headers */
 #include <string>
 #include <vector>
 
@@ -11,12 +14,12 @@ class TPolynomial
     TPolynomial(int);
     ~TPolynomial();
 
-    int deg{}; // stopień wyrażenia wielomianowego
-    std::string phrase{}; // łańcuch znaków przekazywany do obiektu klasy TF1 w celu obliczenia regresji wielomianowej
-    TF1* f{nullptr}; // klasa z biblioteki ROOT pozwalająca na tworzenie funkcji jednowymiarowych
-    std::vector<double> parameters{}; // wektor przechowujący parametry wyrażenia wielomianowego
-    double chi2{}; // parametr wyrażający stopień dopasowania regresji do zbioru danych
-    int ndf{}; // liczba stopni swobody wyrażenia wielomianowego
+    int deg{}; // polynomial expression degree
+    std::string phrase{}; // string passed to a class TF1 object in order to compute the polynomial regression
+    TF1* f{nullptr}; // TF1 (one-dimensional function) class from the ROOT Framework
+    std::vector<double> parameters{}; // vector holding the parameters of a polynomial expression
+    double chi2{}; // fit parameter
+    int ndf{}; // number of degrees of freedom
 };
 
 #endif
